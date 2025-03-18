4793f8aff4e28c4150bcfca3f1f0b294a3c399a7
konevskiysemyon <konevskiy7on@gmail.com>
2025-03-18 10:19:04 +0400

    added github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/black-square',
+  assetPrefix: '/black-square/',
 };
 
 export default nextConfig;
